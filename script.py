d417e797cfb3935af7c677e3ee7e2d1026268c60
bing chen <bingchen2016@gmail.com>
2020-04-01 21:35:27 -0400

    remove changes

--- script.py
+++ script.py
@@ -1,17 +1,5 @@
-import sys
-
 import requests
 
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "hello, {}".format(who_to_greet)
-    return greeting
-
-
 r = requests.get("https://bjex.ca")
-print(greet("World"))
-print(greet("Bing"))
 print(r.status_code)
+print(r.ok)
